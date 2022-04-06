39523c94ecae49c6ab5741c069f4e6c44a378893
Vishnu Prasad K <vishnup@maveric-systems.com>
2022-04-06 20:58:56 +0530

    small change in api url for comment service

--- src/main/java/com/mavericsystems/gatewayservice/config/GatewayConfig.java
+++ src/main/java/com/mavericsystems/gatewayservice/config/GatewayConfig.java
@@ -1,6 +1,6 @@
 package com.mavericsystems.gatewayservice.config;
 
-import com.mavericsystems.gatewayservice.filter.JwtAuthenticationFilter;
+import com.mavericsystems.gatewayservice.filter.JWTFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -9,27 +9,16 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
-//    @Autowired
-//    JwtAuthenticationFilter filter;
+    @Autowired
+    JWTFilter filter;
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder routeLocatorBuilder) {
-//        return routeLocatorBuilder.routes().route("authentication-authorization-service", r -> r.path("/auth/**").filters(f -> f.filter(filter)).uri("http://localhost:3000/"))
-//                .route("user-service", r -> r.path("/users/**").filters(f -> f.filter(filter)).uri("http://localhost:3005/"))
-//                .route("post-service", r -> r.path("/posts/**").filters(f -> f.filter(filter)).uri("http://localhost:3010/"))
-//                .route("comment-service", r -> r.path("/comment/**").filters(f -> f.filter(filter)).uri("http://localhost:3015/"))
-//                .route("like-service", r -> r.path("/postsOrComments/**").filters(f -> f.filter(filter)).uri("http://localhost:3020/")).build();
+        return routeLocatorBuilder.routes().route("authentication-authorization-service", r -> r.path("/auth/**").filters(f -> f.filter(filter)).uri("http://localhost:3000/"))
+                .route("user-service", r -> r.path("/users/**").filters(f -> f.filter(filter)).uri("http://localhost:3005/"))
+                .route("comment-service", r -> r.path("/posts/*/comments/**").filters(f -> f.filter(filter)).uri("http://localhost:3015/"))
+                .route("post-service", r -> r.path("/posts/**").filters(f -> f.filter(filter)).uri("http://localhost:3010/"))
+                .route("like-service", r -> r.path("/postsOrComments/**").filters(f -> f.filter(filter)).uri("http://localhost:3020/")).build();
 
-        return routeLocatorBuilder.routes()
-                .route("user-service", rt -> rt.path("/users/**")
-                        .uri("http://localhost:3005/"))
-                .route("comment-service", rt -> rt.path("/posts/*/comments/**")
-                        .uri("http://localhost:3015/"))
-                .route("post-service", rt -> rt.path("/posts/**")
-                        .uri("http://localhost:3010/"))
-                .route("like-service", rt -> rt.path("/postsOrComments/**")
-                        .uri("http://localhost:3020/"))
-                .route("authentication-authorization-service", rt -> rt.path("/auth/**")
-                        .uri("http://localhost:3000/"))
-                .build();
+
     }
 }
---
+++ src/main/java/com/mavericsystems/gatewayservice/model/JWTResponse.java
@@ -0,0 +1,14 @@
+package com.mavericsystems.gatewayservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JWTResponse {
+
+    private String jwtToken;
+
+}
